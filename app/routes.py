ba71dc3a04dcb576d0db083c424e94dbb1ffb390
kristianjackson <kristian.jackson@gmail.com>
2020-07-14 10:54:47 -0400

    Finished Chapter1

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Kristian'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>
+    '''

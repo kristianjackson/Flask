8f28933994552b52ab49c8cd8d6c40eacd2ff80e
kristianjackson <kristian.jackson@gmail.com>
2020-07-14 11:23:57 -0400

    Chapter2 complete

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 
@@ -5,13 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Kristian'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Waldorf!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

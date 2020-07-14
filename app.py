f1d7888eb6b23bd81451103e1d4701a2d606d1fc
kristianjackson <kristian.jackson@gmail.com>
2020-07-14 07:55:22 -0400

    completed the tutorial

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def get_post(post_id):
     conn = get_db_connection()
@@ -16,8 +18,10 @@ def get_post(post_id):
         abort(404)
     return post
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'qwerty12345asdfg12345zxcvb'
+
 
 @app.route('/')
 def index():
@@ -26,10 +30,12 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -45,5 +51,36 @@ def create():
                          (title, content))
             conn.commit()
             conn.close()
-            
+
     return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
